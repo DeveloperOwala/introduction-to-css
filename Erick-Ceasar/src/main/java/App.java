735940cbdf07227d35fa8ce64ffc5439d15facc0
DeveloperOwala <otienoerick46@gmail.com>
2021-09-06 13:18:25 +0300

    ceasar app

--- Erick-Ceasar/src/main/java/App.java
+++ Erick-Ceasar/src/main/java/App.java
@@ -2,22 +2,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main (String [] args){
-        Scanner myScanner = new Scanner(System.in);
+        Scanner object = new Scanner(System.in);
         boolean runProgram =true;
         while (runProgram){
             System.out.println("Welcome to Caesar Cipher");
             System.out.println("**************");
             System.out.println("Select Encrypt or Decrypt or Exit to leave App");
 
-            String navigationChoice =myScanner.nextLine();
+            String navigationChoice =object.nextLine();
 
             if (navigationChoice.equals("Encrypt")){
                 System.out.println("Enter Message you want to Encrypt? ");
-                String input =myScanner.nextLine();
+                String encryptInput =object.nextLine();
 
                 System.out.println("Enter key");
-                int key =myScanner.nextInt();
-                Cipher caesar =new Cipher(input ,key);
+                int key =object.nextInt();
+                Cipher caesar =new Cipher(encryptInput ,key);
                 String encrypt =Cipher.encrypt(caesar);
                 System.out.println("ENCRYPTED MESSAGE:"+encrypt);
 
@@ -25,9 +25,9 @@ public class App {
             }else if(navigationChoice.equals("Decrypt")){
 
                 System.out.println("Enter the message to decrypt");
-                String decode = myScanner.nextLine();
+                String decode = object.nextLine();
                 System.out.println("Enter key");
-                int key =myScanner.nextInt();
+                int key =object.nextInt();
                 Cipher caesar =new Cipher(decode,key);
                 String decrypt =Cipher.decrypt(caesar);
                 System.out.println("DECRYPTED MESSAGE:"+decrypt);
